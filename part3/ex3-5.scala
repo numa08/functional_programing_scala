59c69b50eaf67422b369ef25369929652fe7584b
numa08 <n511287@gmail.com>
2015-06-23 07:53:43 +0900

    ex3-5

---
+++ part3/ex3-5.scala
@@ -0,0 +1,8 @@
+def dropWile[A](l : List[A], f : A => Boolean) : List[A] = l match {
+  case Cons(h, t) if f(h) => dropWile(t, f)
+  case _ => l
+}
+
+def testDropWile() : Unit = {
+  println(s"dropWile(List(1,2,3,4,5), (x : Int) => x < 4) == List(4,5) ${dropWile(List(1,2,3,4,5), (x : Int) => x < 4) == List(4,5)}")
+}
