e68a7cb35ac272aa7e42be5320ff3b6d050e2cd2
numa08 <n511287@gmail.com>
2015-08-24 08:26:19 +0900

    fix bug

--- part3/List.scala
+++ part3/List.scala
@@ -20,7 +20,7 @@ object List {
     else Cons(as.head, apply(as.tail: _*))
   }
 
-  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B = 
+  def foldRight[A,B](as: List[A], z: B)(f: (A, B) => B): B =
     as match {
       case Nil => z
       case Cons(x, xs) => f(x, foldRight(xs, z)(f))
@@ -40,21 +40,9 @@ object List {
     case _ => 101
   }
 
-  def append[A](a1 : List[A], a2 : List[A]) : List[A] = 
+  def append[A](a1 : List[A], a2 : List[A]) : List[A] =
     a1 match {
       case Nil => a2
       case Cons(h, t) => Cons(h, append(t, a2))
     }
-  
-
-  def foldRight[A, B](as : List[A], z : B)(f : (A, B) => B) : B = {
-    as match {
-      case Nil => z
-      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
-    }
-  }
-
-  def sum2(ns : List[Int]) = foldRight(ns, 0)((x, y) => x + y)
-
-  def product2(ns : List[Double]) = foldRight(ns, 1.0)(_ * _)
 }
